05cbadb136d24102b5f9da4fbdc3fcf529f56328
shassaninia <xai.soft80@gmail.com>
2017-02-02 10:18:22 -0500

    Add webpack.config.js to hold webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    //define entry point
+    entry: "./src/script1.js",
+
+    //define output point
+    output: {
+        // folder and file will be created automatically when webpack is run.
+        path: "dist", //
+        filename: "bundle.js"
+    }
+
+}
