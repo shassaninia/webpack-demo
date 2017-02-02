790a9c483d14ecee043d8d80b7456d86f6e8d873
shassaninia <xai.soft80@gmail.com>
2017-02-02 10:29:09 -0500

    Add const to script2

--- src/script2.js
+++ src/script2.js
@@ -1,1 +1,4 @@
-module.exports = "Hello, Webpack"; //When someone requires you, give them this string
+const msg = "Hello, Webpack"; //const is ES2015 (we need a loader to transform this to javascrip
+//the browser understands)
+
+module.exports = msg; //When someone requires you, give them this string
